cac6b3b4348e62a5d962223fc65e715af4449062
Your Name <you@example.com>
2022-11-10 18:11:55 +0100

    "kommentare"

---
+++ src/main/java/com/r4u/parameters/repositories/ContactRepository.java
@@ -0,0 +1,10 @@
+package com.r4u.parameters.repositories;
+
+import com.r4u.parameters.models.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContactRepository extends JpaRepository<Contact, Integer> {
+
+}
---
+++ src/main/java/com/r4u/parameters/services/ModuleService.java
@@ -0,0 +1,4 @@
+package com.r4u.parameters.services;
+
+public class ModuleService {
+}
